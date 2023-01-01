cc0258edd5a96fda36a8356b1ac28249c517eb7a
Nagico <yjr888@vip.qq.com>
2023-01-01 21:59:51 +0800

    feat(test:views): 添加APIRoot单测

---
+++ tests/test_views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.hashers import make_password
+from model_bakery import baker
+from rest_framework.test import APITestCase
+
+from tests.models import User
+
+
+class APIRootViewSetTestCase(APITestCase):
+    def setUp(self):
+        self.username = "foo"
+        self.password = "bar"
+        self.user = baker.make(
+            User,
+            is_active=True,
+            username=self.username,
+            password=make_password(self.password),
+        )
+
+    def test_anonymous(self):
+        data = self.client.get("").data
+        res = dict(
+            user=dict(
+                id=None,
+                username=None,
+                is_active=None,
+                is_superuser=None,
+            ),
+            time=data["time"],
+        )
+        self.assertDictEqual(data, res)
+
+    def test_authenticated(self):
+        self.client.login(username=self.username, password=self.password)
+        data = self.client.get("").data
+        res = dict(
+            user=dict(
+                id=self.user.pk,
+                username=self.user.username,
+                is_active=self.user.is_active,
+                is_superuser=self.user.is_superuser,
+            ),
+            time=data["time"],
+        )
+        self.assertDictEqual(data, res)
